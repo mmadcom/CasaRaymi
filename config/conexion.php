75dea311fb35288ca7a2c34a1cfd2299313d07f5
rodrigoninaa <ninarodrigo6@gmail.com>
2024-12-04 20:19:09 -0500

    act

--- config/conexion.php
+++ config/conexion.php
@@ -1,22 +1,4 @@
 <?php
-<<<<<<< HEAD
-    $host = "localhost";
-    $user = "root";
-    $clave = "";
-<<<<<<< HEAD
-    $bd = "bd_raymi";
-=======
-    $bd = "bd_shuriken";
->>>>>>> 828ff8d (commit inicial - proyecto funcional)
-    $conexion = mysqli_connect($host,$user,$clave,$bd);
-    if (mysqli_connect_errno()){
-        echo "No se pudo conectar a la base de datos";
-        exit();
-    }
-    
-    mysqli_select_db($conexion,$bd) or die("No se encuentra la base de datos");
-    mysqli_set_charset($conexion,"utf8");
-=======
 // Obtener los valores de las variables de entorno
 $servername = getenv('MYSQLHOST');  // Esto es el host de la base de datos
 $username = getenv('MYSQLUSER');    // Nombre de usuario para la base de datos
@@ -33,4 +15,3 @@ if (!$conn) {
 
 echo "Connected successfully";
 ?>
->>>>>>> ca1c3bf (actualizacion conexion.php)
